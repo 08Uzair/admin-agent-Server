d3cdc9fc5b153743a3d9b599d71698144c554ae3
08Uzer <121750611+08Uzair@users.noreply.github.com>
2025-05-18 15:14:25 +0530

    third commit

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const URI =
-  "mongodb+srv://adminDB:adminDB@cluster0.josyexb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://avez3npqureshi:ecomm@cluster0.iqgv1yt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 export const dataBaseConnection = async () => {
   try {
